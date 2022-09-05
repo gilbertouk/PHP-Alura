6d370d44b5d71908e4e9992d5b2d407754518e68
Gilberto Antonio <103536077+gilbertouk@users.noreply.github.com>
2022-09-05 18:48:23 +0100

    Studying PHP

---
+++ 02-Avancando-com-PHP-Arrays-Strings-Funcao-e-Web/loop-lista.php
@@ -0,0 +1,7 @@
+<?php
+    $idadeList = [21, 23, 19, 25, 30, 41, 18, 15, 15, 28, 52, 68, 48];
+    
+    for ($i = 0; $i < count($idadeList); $i++) {
+        echo $idadeList[$i] . "<br>";
+    }
+?>
