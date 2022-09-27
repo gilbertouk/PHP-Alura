bb5159f0188623a60d142d68c784be100e4ac157
Gilberto Antonio <103536077+gilbertouk@users.noreply.github.com>
2022-09-27 15:47:07 +0100

    Studying PHP

--- 06-PHP-Exceptions-Tratamento-de-erros/pilha.php
+++ 06-PHP-Exceptions-Tratamento-de-erros/pilha.php
@@ -2,6 +2,15 @@
     function funcao1()
     {
         echo "Entrei na funcao 1 <br>";
+        try {
+
+            $arrayFixo = new SplFixedArray(2);
+            $arrayFixo[3] = 'Valor';
+            /*$divisao = intdiv(5, 0);*/
+        } catch (RuntimeException $problema){
+            echo "Aconteceu um erro na funcao 1. <br>";
+            echo $problema . "<br>";
+        }
         funcao2();
         echo "Saindo da funcao 1 <br>";
     }
