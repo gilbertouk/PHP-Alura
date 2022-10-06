66a6ed9a8588fe96519b98b20cdf07091b6c9377
Gilberto Antonio <103536077+gilbertouk@users.noreply.github.com>
2022-10-06 20:41:26 +0100

    Studying PHP

--- 08-PHP-e-PDO-Trabalhando-com-bancos-de-dados/inserir-aluno.php
+++ 08-PHP-e-PDO-Trabalhando-com-bancos-de-dados/inserir-aluno.php
@@ -1,19 +1,41 @@
 <?php
 
 use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
 
 require_once 'vendor\autoload.php';
 
-$pdo = \Alura\Pdo\Infrastructure\Persistence\ConnectionCreator::createConnection();
+$pdo = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($pdo);
 
-$student = new Student(null, 'Patricia Freitas', new DateTimeImmutable('1994-02-20'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
-$statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(':name', $student->name());
-$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
-$statement->execute();
+$pdo->beginTransaction();
 
-if ($statement->execute()) {
+try {
+    $student = new Student(
+        null,
+        'Gilberto Antonio',
+        new DateTimeImmutable('1991-01-01')
+    );
+    $studentRepository->save($student);
+    $pdo->commit();
+
+} catch (\PDOException $e) {
+    echo $e->getMessage() . PHP_EOL;
+    echo $e->errorInfo[2];
+    $pdo->rollBack();
+}
+/**
+ *
+ * $sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
+ * $statement = $pdo->prepare($sqlInsert);
+ * $statement->bindValue(':name', $student->name());
+ * $statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
+ * $statement->execute();
+ *
+ */
+
+if ($studentRepository->save($student)) {
     echo "Aluno incluido.";
 }
--- 08-PHP-e-PDO-Trabalhando-com-bancos-de-dados/src/Infrastructure/Persistence/ConnectionCreator.php
+++ 08-PHP-e-PDO-Trabalhando-com-bancos-de-dados/src/Infrastructure/Persistence/ConnectionCreator.php
@@ -8,9 +8,15 @@ class ConnectionCreator
 {
     public static function createConnection(): PDO
     {
-        $databasePath = __DIR__ . '/../../../banco.sqlite';
+        /**
+         * DADOS DE CONEXAO PARA O BANCO SQLITE
+         *
+         * $databasePath = __DIR__ . '/../../../banco.sqlite';
+         * $connection = $pdo = new PDO('sqlite:' . $databasePath);
+         *
+        */
 
-        $connection = $pdo = new PDO('sqlite:' . $databasePath);
+        $connection = new PDO('mysql:host=192.168.0.250:3306;dbname=bd_alura', 'gilberto', '123456');
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
