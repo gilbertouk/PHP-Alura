fce3954a7ba011dd7189b94efc4db13996bab6ec
Gilberto Antonio <103536077+gilbertouk@users.noreply.github.com>
2022-10-18 13:18:16 +0100

    Studying PHP

--- 09-Doctrine-ORM-Acesse-o-banco-com-Mapeamento-Objeto-Relacional/commands/criar-aluno.php
+++ 09-Doctrine-ORM-Acesse-o-banco-com-Mapeamento-Objeto-Relacional/commands/criar-aluno.php
@@ -1,12 +1,21 @@
 <?php
 
 use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $aluno = new Aluno();
 $aluno->setNome($argv[1]);
+
+for ($i = 2; $i < $argc; $i++) {
+    $numeroTelefone = $argv[$i];
+    $telefone = new Telefone();
+    $telefone->setNumero($numeroTelefone);
+
+    $aluno->addTelefone($telefone);
+}
 
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
--- 09-Doctrine-ORM-Acesse-o-banco-com-Mapeamento-Objeto-Relacional/src/MyProject/Migrations/Version20221018121145.php
+++ 09-Doctrine-ORM-Acesse-o-banco-com-Mapeamento-Objeto-Relacional/src/MyProject/Migrations/Version20221018121145.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221018120447 extends AbstractMigration
+final class Version20221018121145 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,7 @@ final class Version20221018120447 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE Aluno (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome VARCHAR(255) NOT NULL)');
         $this->addSql('CREATE TABLE Telefone (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, aluno_id INTEGER DEFAULT NULL, numero VARCHAR(255) NOT NULL, CONSTRAINT FK_D8448137B2DDF7F4 FOREIGN KEY (aluno_id) REFERENCES Aluno (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_D8448137B2DDF7F4 ON Telefone (aluno_id)');
     }
@@ -27,6 +28,7 @@ final class Version20221018120447 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE Aluno');
         $this->addSql('DROP TABLE Telefone');
     }
 }
