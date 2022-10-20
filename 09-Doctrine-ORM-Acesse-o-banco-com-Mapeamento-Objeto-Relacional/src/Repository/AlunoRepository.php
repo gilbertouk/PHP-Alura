3f444dda7f697697ce979dc0e52177816d41b206
Gilberto Antonio <103536077+gilbertouk@users.noreply.github.com>
2022-10-20 13:21:48 +0100

    Studying PHP

--- 09-Doctrine-ORM-Acesse-o-banco-com-Mapeamento-Objeto-Relacional/src/Repository/AlunoRepository.php
+++ 09-Doctrine-ORM-Acesse-o-banco-com-Mapeamento-Objeto-Relacional/src/Repository/AlunoRepository.php
@@ -7,12 +7,24 @@ use Doctrine\ORM\EntityRepository;
 
 class AlunoRepository extends EntityRepository
 {
-    public function buscaCursosPorAluno()
+    public function buscaCursosPorAlunoOld()
     {
         $classeAluno = Aluno::class;
         $entityManager = $this->getEntityManager();
         $dql = "SELECT aluno, telefones, cursos FROM $classeAluno aluno JOIN aluno.telefones telefones JOIN aluno.cursos cursos";
         $query = $entityManager->createQuery($dql);
+
+        return $query->getResult();
+    }
+
+    public function buscaCursosPorAluno()
+    {
+        $query = $this->createQueryBuilder('a')
+            ->join('a.telefones', 't')
+            ->join('a.cursos', 'c')
+            ->addSelect('t')
+            ->addSelect('c')
+            ->getQuery();
 
         return $query->getResult();
     }
